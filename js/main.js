39264a67d9540cff5ac11476f3282e8345df4277
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2021-03-23 23:49:21 +0600

    Main JavaScript
    main JavaScript code

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ /**
+ main js
+ **/
+
+ $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         
+         $(".zoom").hover(function(){
+         
+         $(this).addClass('transition');
+         }, function(){
+         
+         $(this).removeClass('transition');
+         });
+         });
